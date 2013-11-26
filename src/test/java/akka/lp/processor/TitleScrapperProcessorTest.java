e1c15785c705374c6c42b01fdd3c0651864c62a5
Dmitriy Chizhikov <dmytro.chyzhykov@yandex.ru>
2013-11-26 17:08:39 +0200

    More unit tests for the Title Scrapper processor

--- src/test/java/akka/lp/processor/TitleScrapperProcessorTest.java
+++ src/test/java/akka/lp/processor/TitleScrapperProcessorTest.java
@@ -3,14 +3,16 @@ package akka.lp.processor;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.lp.domain.Generator;
-import akka.testkit.TestActorRef;
 import akka.pattern.Patterns;
-import org.junit.After;
+import akka.testkit.JavaTestKit;
+import akka.testkit.TestActorRef;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
@@ -18,21 +20,25 @@ import scala.concurrent.duration.Duration;
 
 public class TitleScrapperProcessorTest {
 
-    private ActorSystem system;
+    private static ActorSystem system;
     private TitleScrapperProcessor tileScrapper;
     private TestActorRef<TitleScrapperProcessor> tileScrapperRef;
 
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        system = ActorSystem.create("MyUnitTestSystem");
+    }
+
     @Before
     public void setUp() throws Exception {
-        system = ActorSystem.create("MyUnitTestSystem");
         Props props = Props.create(TitleScrapperProcessor.class);
         tileScrapperRef = TestActorRef.create(system, props, "TestTitleScrapper");
         tileScrapper = tileScrapperRef.underlyingActor();
     }
 
     @Test
-    public void testName() throws Exception {
-        Generator message = new Generator("id", "http://apple.com");
+    public void itShouldReturnGeneratorWithScrappedTitle() throws Exception {
+        Object message = new Generator("id", "http://apple.com");
 
         Future<Object> future = Patterns.ask(tileScrapperRef, message, 3000L);
         assertTrue(future.isCompleted());
@@ -41,9 +47,19 @@ public class TitleScrapperProcessorTest {
         assertEquals(expected, Await.result(future, Duration.Zero()));
     }
 
-    @After
-    public void tearDown() throws Exception {
-        system.shutdown();
+    @Ignore("It fails") // TODO: Find how to make it work
+    @Test
+    public void itShouldNotReturnAnything() throws Exception {
+        Object message = "SHOULD_NOT_BE_HANDLED";
+
+        Future<Object> future = Patterns.ask(tileScrapperRef, message, 3000L);
+        assertTrue(future.isCompleted());
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        JavaTestKit.shutdownActorSystem(system);
+        system = null;
     }
 
 }
